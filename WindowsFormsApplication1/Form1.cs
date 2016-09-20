cd5ef86d2956616781d3acafda68d088fec9115b
TheBlade7c <rik_7@msn.com>
2016-09-20 15:18:43 +0200

    test

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -7,14 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        SqlConnection myConnection;
+
         public Form1()
         {
             InitializeComponent();
         }
+        
+
+        private void ButConnect_Click(object sender, EventArgs e)
+        {
+            /*
+            myConnection = new SqlConnection("user id=" + BoxUser.Text +
+                                                   ";password="+ BoxPassword.Text +";server=" + BoxServer.Text +
+                                                   ";Trusted_Connection=yes" +
+                                                   ";database=app" +
+                                                   ";connection timeout=30");
+            */
+            myConnection = new SqlConnection("user id=root;" +
+                                       "password=;server=localhost;" +
+                                       "Trusted_Connection=True;" +
+                                       "database=app; " +
+                                       "connection timeout=30");
+            Connect();
+
+            //test();
+                
+            
+
+        }
+        private void Connect()
+        {
+            try
+            {
+                myConnection.Open();
+                ButConnect.BackColor = System.Drawing.Color.LightGreen;
+            }
+            catch (Exception e)
+            {
+                ButConnect.BackColor = System.Drawing.Color.Red;
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private void test()
+        {
+            try
+            {
+                SqlDataReader myReader = null;
+                SqlCommand myCommand = new SqlCommand("select * from questions",
+                                                         myConnection);
+                myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    System.Diagnostics.Debug.WriteLine(myReader["Column1"].ToString());
+                    System.Diagnostics.Debug.WriteLine(myReader["Column2"].ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private void close()
+        {
+            try
+            {
+                myConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private void Form1_Closed(object sender, EventArgs e)
+        {
+            close();
+        }
+        
     }
 }
